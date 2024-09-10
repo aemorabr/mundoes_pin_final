ae12e92ef0ead963dd8376583163dbeda114b9cd
Mora Brenes, Allan <allan.mora.brenes@intel.com>
2024-09-10 11:41:55 -0700

    Fluen Bit

--- 03_fluentbit/00_seteovariables.sh
+++ 03_fluentbit/00_seteovariables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export AWS_REGION="us-east-2"
-export ACCOUNT_ID="813559548525"
+export AWS_REGION="us-east-1"
+export ACCOUNT_ID="975050077248"
 export ES_DOMAIN_NAME="eksworkshop-logging"
 export ES_VERSION="7.4"
 export ES_DOMAIN_USER="eksworkshop"
@@ -9,4 +9,4 @@ export ES_DOMAIN_PASSWORD="$(openssl rand -base64 12)_Ek1$"
 
 # OIDC del cluster de elasticsearch
 #
-eksctl utils associate-iam-oidc-provider --cluster mundoes-cluster-G6  --approve
+eksctl utils associate-iam-oidc-provider --cluster eks-mundos-e  --approve
